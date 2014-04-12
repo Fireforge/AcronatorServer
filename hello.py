a305bc3a070f42d11639bd9e2eba4f9080ec952a
exfoxz <chanhnp@gmail.com>
2014-04-12 18:54:33 -0400

    post

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask.ext import restful
 from flask.ext.restful.utils import cors
 from flask.ext.restful import reqparse
+import test
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -10,12 +11,16 @@ api = restful.Api(app)
 class HelloWorld(restful.Resource):
     def get(self):
         return {'hello': 'world'}, 200, {'Access-Control-Allow-Origin': '*'}
+
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('name', type=str)
         parser.add_argument('des', type=str)
         args = parser.parse_args()
-        return {'name': args['name'], 'des': args['des']}, 200, {'Access-Control-Allow-Origin': '*'}
+
+        results = test.acronym_finder(args['name'],5,args['des'])
+
+        return {'results': results}, 200, {'Access-Control-Allow-Origin': '*'}
 
 api.add_resource(HelloWorld, '/')
 
