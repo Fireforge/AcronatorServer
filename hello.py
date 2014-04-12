a42eae9aa103f8e856a193c137693c0200c1ff04
exfoxz <chanhnp@gmail.com>
2014-04-12 18:27:10 -0400

    jsonify

--- hello.py
+++ hello.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 api = restful.Api(app)
 
 class HelloWorld(restful.Resource):
+    @cross_origin()
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+@cross_origin()
